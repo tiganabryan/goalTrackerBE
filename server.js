4ab8996d4dfbca89f91f14331e8caee184289bae
Tigana Bryan <tiganabusinessinquiries@gmail.com>
2022-12-22 11:10:14 -0500

    add goalTrackerRouter

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const app = express();
 
 const log = console.log;
 
-app.get("/api/goalTracker", (req, res) => {
-	res.status(200).json({ "test goal": "drink 3L water" });
-});
+// app.get("/api/goalTracker", (req, res) => {
+// 	res.status(200).json({ "test goal": "drink 3L water" });
+// });
+
+app.use("/api/goalTracker", require("./routes/goalTrackerRoutes"));
 
 app.listen(PORT, () => log(`now listening on port ${PORT}:)`));
