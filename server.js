7b05afab80570e79727f853a79098e66c478e3a3
Tigana Bryan <tiganabusinessinquiries@gmail.com>
2022-12-22 10:47:57 -0500

    add handler for get requests to /api/goalTracker

--- server.js
+++ server.js
@@ -1,25 +1,13 @@
 const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 5000;
 
 const express = require("express");
 const app = express();
 
-const mongoose = require("mongoose");
-
-const PORT = process.env.PORT || 5000;
-
 const log = console.log;
 
+app.get("/api/goalTracker", (req, res) => {
+	res.json({ "test goal": "drink 3L water" });
+});
+
 app.listen(PORT, () => log(`now listening on port ${PORT}:)`));
-
-// mongoose.connect(process.env.DATABASE_URL);
-
-// const db = mongoose.connect("mongodb://localhost:27017/goalTracker");
-
-// db.find();
-
-// db.on("error", (err) => log(err));
-// db.once("open", () => log("connected to db:)"));
-
-// mongoose.set("strictQuery", true);
-
-//[MONGOOSE] DeprecationWarning: Mongoose: the `strictQuery` option will be switched back to `false` by default in Mongoose 7. Use `mongoose.set('strictQuery', false);` if you want to prepare for this change. Or use `mongoose.set('strictQuery', true);` to suppress this warning.
