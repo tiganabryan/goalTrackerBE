0223581e0a13f52ef607a7d42846d225ce4cdae0
Tigana Bryan <tiganabusinessinquiries@gmail.com>
2022-12-23 22:13:47 -0500

    create userSchema

---
+++ models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+	{
+		firstName: {
+			type: String,
+			required: [true, "please add your first name"],
+		},
+		username: {
+			type: String,
+			required: true,
+			unique: [true, "please add a username"],
+		},
+		email: {
+			type: String,
+			required: [true, "please add your email"],
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+module.exports = mongoose.model("users", userSchema);
