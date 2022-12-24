8694b2811155beba52d181679ecbf52fc3378b06
Tigana Bryan <tiganabusinessinquiries@gmail.com>
2022-12-23 20:39:43 -0500

    update db with urlencoded!
    await Goal.create() when we get a post req

--- models/goalModel.js
+++ models/goalModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const goalSchema = mongoose.Schema(
+const goalSchema = new Schema(
 	{
 		text: {
 			type: String,
@@ -8,8 +9,14 @@ const goalSchema = mongoose.Schema(
 		},
 	},
 	{
+		achieved: {
+			type: Number | Boolean,
+			default: false,
+		},
+	},
+	{
 		timestamps: true,
 	}
 );
 
-module.exports = mongoose.model("Goal", goalSchema);
+module.exports = mongoose.model("goals", goalSchema);
--- routes/controllers/goalTrackerController.js
+++ routes/controllers/goalTrackerController.js
@@ -1,12 +1,14 @@
 const asyncHandler = require("express-async-handler");
+const mongoose = require("mongoose");
 const log = console.log;
 
-const goal = require("../../models/goalModel");
+const Goal = require("../../models/goalModel");
 
 // route: GET /api/goalTracker
 // access: public (for now)
 const getGoals = asyncHandler(async (req, res) => {
-	res.status(200).json({ task: "get goals" });
+	const goals = await Goal.find({});
+	res.status(200).json(goals);
 });
 
 // route: POST /api/goalTracker
@@ -17,7 +19,10 @@ const addGoal = asyncHandler(async (req, res) => {
 		res.status(400);
 		throw new Error("please add a text field");
 	}
-	res.status(200).json({ task: "add a goal" });
+	const goal = await Goal.create({
+		text: req.body.text,
+	});
+	res.status(200).json(goal);
 });
 
 // route: PUT /api/goalTracker
