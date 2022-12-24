927a79ff47467215ef696a930387502e79dfd284
Tigana Bryan <tiganabusinessinquiries@gmail.com>
2022-12-23 19:23:16 -0500

    add asyncHandler to controller functions

--- routes/controllers/goalTrackerController.js
+++ routes/controllers/goalTrackerController.js
@@ -1,34 +1,34 @@
+const asyncHandler = require("express-async-handler");
 const log = console.log;
 
 // route: GET /api/goalTracker
 // access: public (for now)
-const getGoals = async (req, res) => {
-	if (!req.body.text) {
-		res.status(400);
-		throw new Error("pls add a text field");
-	} else {
-		res.status(200).json({ task: "get goals" });
-	}
-};
+const getGoals = asyncHandler(async (req, res) => {
+	res.status(200).json({ task: "get goals" });
+});
 
 // route: POST /api/goalTracker
 // access: public (for now)
-const addGoal = async (req, res) => {
+const addGoal = asyncHandler(async (req, res) => {
 	log(req.body);
+	if (!req.body.text) {
+		res.status(400);
+		throw new Error("please add a text field");
+	}
 	res.status(200).json({ task: "add a goal" });
-};
+});
 
 // route: PUT /api/goalTracker
 // access: public (for now)
-const editGoal = async (req, res) => {
+const editGoal = asyncHandler(async (req, res) => {
 	res.status(200).json({ task: `edit goal: ${req.params.id}` });
-};
+});
 
 // route: DELETE /api/goalTracker
 // access: public (for now)
-const deleteGoal = async (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
 	res.status(200).json({ task: `delete goal ${req.params.id}` });
-};
+});
 
 module.exports = {
 	getGoals,
